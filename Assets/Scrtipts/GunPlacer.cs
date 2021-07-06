318282f0711b28175f1a542b3cbe5ef40b7cfcac
SagiZiv3 <73391391+SagiZiv3@users.noreply.github.com>
2021-07-06 13:36:54 +0300

    Initial commit with models
    Unity project with models and some code

---
+++ Assets/Scrtipts/GunPlacer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPlacer : MonoBehaviour
+{
+    [SerializeField] private List<Transform> hidingSpots;
+    [SerializeField] private List<GameObject> gunModels;
+
+    private void Start()
+    {
+        // Iterate every gun model
+        foreach (GameObject gun in gunModels)
+        {
+            // If don't have any more hiding spots, just exit the loop
+            if (hidingSpots.Count == 0)
+            {
+                Debug.Log("Not enogth hiding spots!");
+                break;
+            }
+            // Select a random hiding spot
+            Transform hidingSpot = hidingSpots[Random.Range(0, hidingSpots.Count)];
+            // Instantiate the gun model at that position
+            Instantiate(gun, hidingSpot.position, Quaternion.identity, hidingSpot);
+            // Remove the hiding spot from the list so no other gun be in it
+            hidingSpots.Remove(hidingSpot);
+        }
+    }
+}
---
+++ Assets/Scrtipts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject gameCamera;
+    private CharacterController characterController;
+    private float speed = 4f, rx = 0, ry = 0;
+    private float angularSpeed = 1f;
+    public Rigidbody rb;
+    private Vector3 motion;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        //characterController = this.GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnDestroy()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dx, dz;
+        // mouse
+        rx -= Input.GetAxis("Mouse Y") * angularSpeed;
+        ry += Input.GetAxis("Mouse X") * angularSpeed;
+  
+        gameCamera.transform.localEulerAngles = new Vector3(rx, 0, 0);
+        transform.localEulerAngles = new Vector3(0, ry, 0);
+
+        dx = Input.GetAxis("Horizontal") * speed;
+        dz = Input.GetAxis("Vertical") * speed ;
+
+        motion = new Vector3(dx, 0, dz);
+        motion = transform.TransformDirection(motion);
+        rb.MovePosition(rb.position + motion);
+
+        //characterController.Move(motion);
+      // transform.Translate(motion);
+
+    }
+}
